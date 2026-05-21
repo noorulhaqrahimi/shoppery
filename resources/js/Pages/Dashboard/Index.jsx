3c03d6a4376bb68dfb7ef2b242bb692763208359
behruz30117 <behrozrezaie8@gmail.com>
2026-05-21 10:53:46 +0430

    Debog: Craate a folder called Dashboard and modified Dashborad.jsx to Index.jsx and moved it to Dashboard folder as it was directed in its route

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,4 +1,4 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AuthenticatedLayout from '@/Components/Layout/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
