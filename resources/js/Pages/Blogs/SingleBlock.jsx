a9565306f6a137f2442e336a3d4e763b244cffa4
amirhussainnaweed <amirhussainnaweed@gmail.com>
2026-06-08 17:18:21 +0430

    feat: worked on single blog

--- resources/js/Pages/Blogs/SingleBlock.jsx
+++ resources/js/Pages/Blogs/SingleBlock.jsx
@@ -3,7 +3,8 @@ import MainLayout from '@/Components/Layout/MainLayout';
 import { Head } from '@inertiajs/react';
 import { MessageSquare, Tag, User } from 'lucide-react';
 import { BsInstagram, BsPinterest, BsTwitter } from 'react-icons/bs';
-import { FaFacebook, FaShareAlt } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { IoLink } from 'react-icons/io5';
 
 const SingleBlock = () => {
     return (
@@ -99,11 +100,52 @@ const SingleBlock = () => {
                                     href="#"
                                     className="flex h-10 w-10 items-center justify-center rounded-full text-gray-600 transition hover:bg-green-600 hover:text-white"
                                 >
-                                    <FaShareAlt size={18} />
+                                    <IoLink size={18} />
                                 </a>
                             </div>
                         </div>
                     </div>
+                    <div className="mt-5 flex flex-col gap-5">
+                        <p className="text-[20px] font-medium leading-[150%] tracking-[0%] text-[#1A1A1A]">
+                            Maecenas lacinia felis nec placerat sollicitudin.
+                            Quisque placerat dolor at scelerisque imperdiet.
+                            Phasellus tristique felis dolor.
+                        </p>
+                        <p className="text-[16px] font-normal leading-[150%] tracking-[0%] text-[#808080]">
+                            Maecenas elementum in risus sed condimentum. Duis
+                            convallis ante ac tempus maximus. Fusce malesuada
+                            sed velit ut dictum. Morbi faucibus vitae orci at
+                            euismod. Integer auctor augue in erat vehicula, quis
+                            fermentum ex finibus.
+                        </p>
+                        <p className="text-[16px] font-normal leading-[150%] tracking-[0%] text-[#808080]">
+                            Mauris pretium elit a dui pulvinar, in ornare sapien
+                            euismod. Nullam interdum nisl ante, id feugiat quam
+                            euismod commodo. Sed ultrices lectus ut iaculis
+                            rhoncus. Aenean non dignissim justo, at fermentum
+                            turpis. Sed molestie, ligula ut molestie ultrices,
+                            tellus ligula viverra neque, malesuada consectetur
+                            diam sapien volutpat risus. Quisque eget tortor
+                            lobortis, facilisis metus eu, elementum est. Nunc
+                            sit amet erat quis ex convallis suscipit. ur
+                            ridiculus mus.
+                        </p>
+                    </div>
+                    <div className="flex justify-between">
+                        <img src="/images/SingleBlog/fruit1.png" alt="" />
+                        <img src="/images/SingleBlog/fruit2.png" alt="" />
+                    </div>
+                    <p className="text-[16px] font-normal leading-[150%] tracking-[0%] text-[#808080]">
+                        Sed dictum non nulla eu imperdiet. Duis elit libero,
+                        vulputate quis vehicula ut, vestibulum ut mauris. Nullam
+                        non felis varius dui rutrum rutrum in a nisi.
+                        Suspendisse elementum rutrum lorem sed luctus. Proin
+                        iaculis euismod metus non sollicitudin. Duis vel luctus
+                        lacus. Nullam faucibus iaculis convallis. In ullamcorper
+                        nibh ipsum, eget lacinia eros pulvinar a. Integer
+                        accumsan arcu nec faucibus ultricies.
+                    </p>
+                    <div className="h-[250px] w-full bg-[url(/images/SingleBlog/rec.png)]"></div>
                 </div>
                 <div></div>
             </div>
