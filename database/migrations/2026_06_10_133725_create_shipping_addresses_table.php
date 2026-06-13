5a2759590cd46f4394dcd26c2639c597135a9a94
Mohammad Bahmani <mohammadmandegar2019@gmail.com>
2026-06-13 15:19:06 +0430

    feat: add migrations

---
+++ database/migrations/2026_06_10_133725_create_shipping_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipping_addresses', function (Blueprint $table) {
+            $table->id();
+               $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+               $table->string('text');
+               $table->string('city');
+               $table->string('country');
+               $table->integer('postal_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipping_addresses');
+    }
+};
