d4c652a220008d18aa19565f487b1ceb2f0a5af5
ahmad001-code <rahimirokhan@gmail.com>
2026-06-16 13:31:03 +0430

    edit: add extra file for popup

--- resources/js/Pages/ProductDetails/Index.jsx
+++ resources/js/Pages/ProductDetails/Index.jsx
@@ -3,19 +3,14 @@ import { AnimatedSection } from '@/Components/Layout/AnimatedSection';
 import MainLayout from '@/Components/Layout/MainLayout';
 import VegetableSlider from '@/Components/Layout/VegetableSlider';
 import { Head } from '@inertiajs/react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 import Counter from './Counter';
+import Popup from './Popup';
 import ProductTabs from './ProductTabs';
 import RelatedCards from './RelatedCards';
 
 function ProductDetails() {
-    const [hidden, setHidden] = useState(false);
-    function HandleShow() {
-        setHidden(true);
-    }
-    let cancel1;
-    let cancel2;
     const stars = [
         { link: '/images/star.png' },
         { link: '/images/star.png' },
@@ -32,118 +27,7 @@ function ProductDetails() {
 
     return (
         <>
-            <AnimatePresence>
-                {showPopup && (
-                    <>
-                        <motion.div
-                            onClick={() => setShowPopup(false)}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.2 }}
-                            className="fixed inset-0 z-[60] bg-black/40 backdrop-blur-sm"
-                        ></motion.div>
-                        <motion.div
-                            initial={{ x: '100%' }}
-                            animate={{ x: 0 }}
-                            exit={{ x: '100%' }}
-                            transition={{
-                                type: 'spring',
-                                stiffness: 130,
-                                damping: 18,
-                            }}
-                            className="fixed right-0 z-[60] h-[100vh] w-[70%] bg-white sm:w-[60%] lg:w-[40%] 2xl:w-[35]"
-                        >
-                            <div className="px-6">
-                                <div className="mt-6 flex h-20 w-full items-center justify-between">
-                                    <h1 className="text-[20px] font-medium">
-                                        Shopping Card (2)
-                                    </h1>
-                                    <motion.button
-                                        whileHover={{ scale: 1.2 }}
-                                        transition={{ duration: 0.3 }}
-                                        onClick={() => setShowPopup(false)}
-                                        className="text-[25px] font-light"
-                                    >
-                                        x
-                                    </motion.button>
-                                </div>
-                                <div className="flex items-center justify-between pb-2">
-                                    <div className="flex items-center">
-                                        <img
-                                            className="h-[100px] w-[120px]"
-                                            src="/images/orange.png"
-                                            alt="Oranges"
-                                        />
-                                        <div>
-                                            <p>Fresh Indian Orange</p>
-                                            <p className="font-semibold">
-                                                <span className="font-normal text-[#808080]">
-                                                    1 kg x
-                                                </span>{' '}
-                                                12.00
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <motion.button
-                                        whileHover={{ scale: 1.2 }}
-                                        transition={{ duration: 0.3 }}
-                                        className="flex h-[24px] w-[24px] items-center justify-center rounded-xl border-[1px] border-gray-300 font-light font-semibold text-[#666666]"
-                                    >
-                                        X
-                                    </motion.button>
-                                </div>
-                                <div
-                                    className={`border-t1 flex items-center justify-between ${cancel2 === hidden ? `hidden` : ``}`}
-                                >
-                                    <div className="flex items-center">
-                                        <img
-                                            className="h-[120px] w-[120px]"
-                                            src="/images/apple.png"
-                                            alt="Oranges"
-                                        />
-                                        <div>
-                                            <p>Green Apple</p>
-                                            <p className="font-semibold">
-                                                <span className="font-normal text-[#808080]">
-                                                    1 kg x
-                                                </span>{' '}
-                                                14.00
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <motion.button className="flex h-[24px] w-[24px] items-center justify-center rounded-xl border-[1px] border-gray-300 font-light font-semibold text-[#666666]">
-                                        X
-                                    </motion.button>
-                                </div>
-                            </div>
-                            <div className="absolute bottom-10 flex w-full flex-col gap-5 px-6">
-                                <div className="flex justify-between">
-                                    <p>2 Product</p>
-                                    <p className="font-bold">$26.00</p>
-                                </div>
-                                <div className="flex flex-col items-center justify-center gap-2">
-                                    <motion.button
-                                        whileHover={{ scale: 1.05 }}
-                                        transition={{ duration: 0.3 }}
-                                        className="h-[50px] w-[260px] rounded-[43px] bg-[#00B207] font-semibold text-white sm:w-[350px] lg:w-[300px]"
-                                    >
-                                        Checkout
-                                    </motion.button>
-                                    <motion.button
-                                        whileHover={{ scale: 1.05 }}
-                                        transition={{ duration: 0.3 }}
-                                        className="h-[50px] w-[260px] rounded-[43px] bg-[#56AC591A] font-semibold text-[#00B207] sm:w-[350px] lg:w-[300px]"
-                                    >
-                                        Go To Cart
-                                    </motion.button>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </>
-                )}
-            </AnimatePresence>
-
+            <Popup showPopup={showPopup} setShowPopup={setShowPopup} />
             <MainLayout>
                 <Breadcrumb
                     items={[
@@ -182,8 +66,8 @@ function ProductDetails() {
                                     </div>
                                     <div className="flex gap-5">
                                         <div className="flex items-center gap-[5px]">
-                                            {stars.map((image) => (
-                                                <div>
+                                            {stars.map((image, index) => (
+                                                <div key={index}>
                                                     <img
                                                         className=""
                                                         src={image.link}
@@ -223,7 +107,7 @@ function ProductDetails() {
                                     </div>
                                     <div className="flex items-center gap-3">
                                         <p>Share items:</p>
-                                        {medias.map((media, index, media2) => (
+                                        {medias.map((media, index) => (
                                             <motion.div
                                                 className="flex h-8 w-8 items-center justify-center rounded-3xl"
                                                 whileHover={{
@@ -233,7 +117,7 @@ function ProductDetails() {
                                                 transation={{ duration: 0.4 }}
                                                 key={index}
                                             >
-                                                <img src={media.link} alt="" />
+                                                <img src={media.link} alt="Social media" />
                                             </motion.div>
                                         ))}
                                     </div>
@@ -259,7 +143,6 @@ function ProductDetails() {
                                     Add to Cart
                                     <img
                                         className="h-4"
-                                        w-4
                                         src="/images/add.png"
                                         alt=""
                                     />
